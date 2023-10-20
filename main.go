1a6a901fcd7a740bf582eae293db9121226b0089
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-20 09:37:55 +0530

    Dummy setup

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/suryaraj1/html-link-parser/util"
 )
 
 func main() {
 	fmt.Println("hello world")
 
+	r := strings.NewReader("./testDocuments/test1.html")
+	links, err := util.Parser(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i, link := range links {
+		fmt.Println(strconv.Itoa(i) + " " + link.Href)
+	}
 }
--- util/parser.go
+++ util/parser.go
@@ -1,1 +1,19 @@
 package util
+
+import "io"
+
+// Type definition for Link
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parser(r io.Reader) ([]Link, error) {
+	links := []Link{
+		{Href: "/example1", Text: "Example Link 1"},
+		{Href: "/example2", Text: "Example Link 2"},
+		{Href: "/example3", Text: "Example Link 3"},
+	}
+
+	return links, nil
+}
